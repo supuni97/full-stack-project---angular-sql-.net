c0d5a5903dee0a032d2657693576eabc18bf5cb9
supuni97 <sitharabandara1997@gmail.com>
2025-06-28 18:19:06 +0530

    Added route for inventory

--- gadget-shop/src/app/app.routes.ts
+++ gadget-shop/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InventoryComponent } from './AppComponents/inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inventory', component: InventoryComponent },
+];
